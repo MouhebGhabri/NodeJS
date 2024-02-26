20676ef5be5e5baa9c920576c02956abd5da6a0d
MouhebGhabri <ghabrimouheb@gmail.com>
2024-02-26 15:48:04 +0100

    .env

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // // 
-// const express = require("express");
+const express = require("express");
 // const voiture = require('./routes/voiture')
-// const app = express();
-// const PORT = 5001;
-// const router = express.Router();
+const app = express();
+const router = express.Router();
 
+const dotenv = require('dotenv');
+dotenv.config()
+const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5001;
 
 
 // const listvoiture = [{"id":1, "name":"clio"},{"id":2, "name":"Porshe Panamera"},{"id":3,"name":"range"}];
@@ -28,3 +31,14 @@
 // app.listen(PORT, ()=> {
 //     console.log("Server")
 // })
+
+const mongo = require('mongoose');
+
+mongo.connect(MONGODB_URI).then(()=>{
+    console.log("connected");
+    app.listen(PORT,()=>{
+        console.log(`server listens on ${PORT}`);
+    })
+}).catch((err)=>{
+    console.log("Error",err.message);
+})
